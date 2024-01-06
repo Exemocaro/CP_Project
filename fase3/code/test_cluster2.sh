1d8662608155e76d90d884542acd2b1d74e2e298
luisAraujo11 <laba_16_araujo@hotmail.com>
2024-01-06 18:30:15 +0000

    cenas

--- fase3/code/test_cluster2.sh
+++ fase3/code/test_cluster2.sh
@@ -4,25 +4,26 @@
 #SBATCH --partition=cpar
 
 # Array of different numbers of processes to test
-processes=(1 2 4 8 10 12 16 20 24 28 32 36 40 43 45 47)
-#processes=(3 5)
+processes=(1 2 4 8 10 12 16 20)
+thread=40
 
 # Loop through each number of processes
 for nprocs in "${processes[@]}"
 do
     echo "Running with ${nprocs} processes:"
-    export OMP_NUM_THREADS=${40}; time mpirun -np ${nprocs} ./MDparmpi.exe < inputdata.txt > output/output_${nprocs}.txt
+    export OMP_NUM_THREADS=${thread}; time mpirun -np ${nprocs} ./MDparmpi.exe < inputdata.txt > output/output_${nprocs}.txt
+    # time mpirun -np ${nprocs} ./MDmpi.exe < inputdata.txt > output/output_${nprocs}.txt
 
-    # cd output
+    cd output
 
-    # diff ../cp_average.txt default_average.txt
-    # diff ../cp_output.txt default_output.txt   
+    diff ../cp_average.txt default_average.txt
+    diff ../cp_output.txt default_output.txt   
 
-    # echo -n "average -> "
-    # diff -y --suppress-common-lines default_average.txt ../cp_average.txt | wc -l
-    # echo -n "output -> "
-    # diff -y --suppress-common-lines default_output.txt ../cp_output.txt | wc -l
-    # cd ..
+    echo -n "average -> "
+    diff -y --suppress-common-lines default_average.txt ../cp_average.txt | wc -l
+    echo -n "output -> "
+    diff -y --suppress-common-lines default_output.txt ../cp_output.txt | wc -l
+    cd ..
     
 done
 
